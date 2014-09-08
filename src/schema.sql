ca6b65ab78785008fa2c485a6cc6b2cee4474cdf
nham <hamann.nick@gmail.com>
2014-09-08 16:01:17 -0400

    Remove trailing commas in SQL schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,15 +1,19 @@
+drop table if exists objects;
+drop table if exists pages;
+drop table if exists page_revisions;
+
 create table objects (
     hash text primary key,
     data text not null
 );
 
 create table pages (
-    id integer primary key autoincrement,
+    id integer primary key autoincrement
 );
 
 create table page_revisions (
     pageid integer,
     rev integer not null,
     hash text not null,
-    FOREIGN KEY(pageid) REFERENCES pages(id),
+    FOREIGN KEY(pageid) REFERENCES pages(id)
 );
