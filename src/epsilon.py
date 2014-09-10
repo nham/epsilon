5e4ce4f71796cd39fe2a884e438cc0789e277288
nham <hamann.nick@gmail.com>
2014-09-10 16:28:46 -0400

    Add initial work on the UI. Very barebones

--- src/epsilon.py
+++ src/epsilon.py
@@ -8,6 +8,10 @@ DATABASE = "epsilon.db"
 app = Flask(__name__)
 
 @app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route('/web')
 def index():
     """Returns JSON data needed to populate the main interface.
 
